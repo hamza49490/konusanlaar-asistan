6484cd35515ebe2ae6e2aecf2863cce39f078086
hamza49490 <164918299+hamza49490@users.noreply.github.com>
2024-04-11 17:12:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", "29462790"))
 API_HASH = getenv("API_HASH", "1fb6a2326a52026dfdb1352f9d065856")
-BOT_TOKEN = getenv("BOT_TOKEN", "6195983007:AAHcSOWhxONqPTCw6dc75010NgWjMf0PR-Y")
+BOT_TOKEN = getenv("BOT_TOKEN", "6195983007:AAHcbmHjxrXpfJ1c0CD2On8en4jj1-TJFig")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Kingbrukh:kingkhan@kingbruh.ra3pjgm.mongodb.net/?retryWrites=true&w=majority")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 500))
 LOGGER_ID = int(getenv("LOGGER_ID", "-1001805137487"))
