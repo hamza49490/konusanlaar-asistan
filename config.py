a8981ac0466be5ce3c0ea687cdf6ec4590cb14d8
hamza49490 <164918299+hamza49490@users.noreply.github.com>
2024-04-14 22:03:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("API_HASH", "1fb6a2326a52026dfdb1352f9d065856")
 BOT_TOKEN = getenv("BOT_TOKEN", "6508386922:AAE-Hbu2yhJLhQwGObXcVhC6cuK3ryLyFY0")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Kingbrukh:kingkhan@kingbruh.ra3pjgm.mongodb.net/?retryWrites=true&w=majority")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 500))
-LOGGER_ID = int(getenv("LOGGER_ID", "-1001805137487"))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002004695942"))
 OWNER_ID = int(getenv("OWNER_ID", 6076070444))
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
